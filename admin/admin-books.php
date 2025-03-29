f3faae2c46351d695f30df8463209dcceab46264
Kawtsun <morpheusjoshua71@gmail.com>
2025-03-29 12:38:46 +0800

    Improve modal layer

--- admin/admin-books.php
+++ admin/admin-books.php
@@ -393,9 +393,9 @@ $conn->close();
             left: 0;
             width: 100%;
             height: 100%;
-            background-color: rgba(0, 0, 0, 0.5);
+            background-color: rgba(0, 0, 0, 0.7);
             /* Semi-transparent background */
-            z-index: 1000;
+            z-index: 1002;
             justify-content: center;
             align-items: center;
         }
